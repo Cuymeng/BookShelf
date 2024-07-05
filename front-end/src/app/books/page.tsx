058dffbff9b5913b52b998fb4ee9ff2beb0afad8
Vangnea <159445563+Cuymeng@users.noreply.github.com>
2024-07-05 12:36:01 +0700

    working_on_link_page

---
+++ front-end/src/app/books/page.tsx
@@ -0,0 +1,6 @@
+export default function books() {
+    return (
+        <h1>Hello Next.js</h1>
+    )
+}
+
---
+++ front-end/src/page/home.tsx
@@ -0,0 +1,7 @@
+const home = () => {
+    return (
+        <h1>Hello Next.js</h1>
+    );
+}
+
+export default home;

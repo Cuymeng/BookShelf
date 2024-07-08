9fa1f6c983dc5bcb21aedd1fc63a0543aa3eabfa
Vangnea <159445563+Cuymeng@users.noreply.github.com>
2024-07-08 15:06:56 +0700

    fixErrorimgtoImage

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import myImg from '@/assets/1234.jpg';
 
 export default function Home() {
@@ -5,16 +6,21 @@ export default function Home() {
     <>
       <div className="hero bg-base-200 min-h-screen " style={{ backgroundColor: '#FCFFE0' }}>
         <div className="hero-content flex-col lg:flex-row-reverse">
-          <img
+
+          <Image
             src={myImg.src}
-            className="max-w-sm rounded-lg shadow-2xl" />
+            alt="Landscape picture"
+            width={500}
+            height={500}
+            className="max-w-sm rounded-lg shadow-2xl"
+          />
           <div>
             <h1 className="text-5xl font-bold">New Release</h1>
             <h2>This Dark Road to mercy</h2>
             <p className="py-6">
               Wade disappeared on us when I was nine years old, and then he showed up out of nowhere the year I turned twelve...
-              After their mother unexpectedly dies, Easter Quilby and her six-year-old sister, Ruby, are stolen away from their foster home by the father they haven't seen in years.
-              Brady Weller, their court-appointed guardian, goes after them, suspecting Wade of having taken part in a recent high-profile robbery. But he's not the only one on their tail, as Robert Pruitt, a mercurial and deadly hunter, is determined to get to them first and claim his due.
+              After their mother unexpectedly dies, Easter Quilby and her six-year-old sister, Ruby, are stolen away from their foster home by the father they have not seen in years.
+              Brady Weller, their court-appointed guardian, goes after them, suspecting Wade of having taken part in a recent high-profile robbery. But he is not the only one on their tail, as Robert Pruitt, a mercurial and deadly hunter, is determined to get to them first and claim his due.
             </p>
             <button className="btn btn-primary">$10 - Purchase</button>
             <button className="btn btn-primary">Add to cart</button>
